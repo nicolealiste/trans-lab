9676ec802ade4a06dbdd45c8fd30268fdcad538b
Nicole Aliste <nicole.alistecastillo@gmail.com>
2017-07-14 10:04:13 -0400

    Se avanza en el Home

--- js/main.js
+++ js/main.js
@@ -1,1 +1,20 @@
 /* Holiii acá va tu código también */
+$(document).ready(function() {
+	
+/*Función para correo*/
+function validarEmail(mail) {
+    var inputCorreo = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (!inputCorreo.test(correo)) {
+        return true;
+    }
+}
+
+/*Función para password*/
+function validaPassword(pass) {
+    var passLenght = pass.length;
+    if (passLenght < 9) {
+        return true;
+    }
+}
+
+});
